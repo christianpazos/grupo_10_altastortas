647e5d171c8c429e7c94137fa62a7ba8587223ea
EmmaCarlos <carlitos.benitez@yahoo.com>
2021-08-09 09:00:18 -0300

    arreglando error de cannot get /usarios/register + multer

--- src/middlewares/multerMiddleware.js
+++ src/middlewares/multerMiddleware.js
@@ -1,19 +1,17 @@
 const multer = require('multer')
 const path = require('path')
 
+module.exports= function (carpetaLoQueVenga){//cualquier carpeta despues de /public/avatars/xxx 
+    const storage = multer.diskStorage({
+        destination:(req,file,cb)=> cb (null, path.resolve(__dirname, '../../public/uploads',carpetaLoQueVenga)), 
+        //con folder mi carpetaLoQueVenga es dinamico, armo la ruta y destino en el uploads
+        filename: (req,file,cb)=> cb (null, file.fieldname + Date.now() + path.extname(file.originalname))
+        //es solo el nombre del archivo
+    });
+    
+    return storage;
 
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './public/img/avatars')
-    },
-    filename: (req, file, cb) => {
-        cb(null, "usuario"+ '-' + file.fieldname + '-' + Date.now()+ path.extname(file.originalname))
-    }
-
-})
-
-module.exports = storage
+}
 
 
 
--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const path = require ('path');
 const multer = require ('multer');
 const usuario = require ('../controllers/usuario')
-const validRegister = require("../middlewares/validRegister");
+const validRegister = require("../middlewares/validationRegister");
 const storage = require("../middlewares/multerMiddleware");//esta con folder para que sea dinamico
-const upload = multer({storage: storage("avatar")}) 
+const uploadFile = multer({storage: storage("avatar")}) 
 
 
 router.get("/login", usuario.login)
@@ -15,7 +15,7 @@ router.get("/register", usuario.register)
 //Formulario de Login
 router.get("/login", usuario.login)
 //Procesar el Registro
-router.post ("/register",[uploadFile.single ('avatar'),validRegister], usuario.save)
+router.post ("/save",[uploadFile.single ('avatar'),validRegister], usuario.save)
 //Formulario de Registro
 
-
+module.exports = router
